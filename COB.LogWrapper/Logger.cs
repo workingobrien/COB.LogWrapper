732e2bde468e2a75c11deeb43570f1bbb29c1ea7
Chris O'Brien <workingobrien@gmail.com>
2015-06-19 05:36:31 -0400

    Initial Checkin

---
+++ COB.LogWrapper/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+namespace COB.LogWrapper
+{
+   public static class Logger
+   {
+      private static readonly ILog _log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+      public static ILog Log
+      {
+         get { return _log; }
+      }
+
+      public static void LogException(Exception ex)
+      {
+         _log.Error(ex.Message, ex);
+      }
+
+   }
+}
